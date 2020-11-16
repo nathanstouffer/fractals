ab4f1e7053f24eebac3052cd2ab7e778f9476480
Nathan Stouffer <nathanstouffer1999@gmail.com>
2020-11-16 13:20:56 -0700

    writing basic mandelbrot set code (colors not smooth)

---
+++ code/v1/fractal.h
@@ -0,0 +1,52 @@
+// interface IFractalGen
+// with method color_complex_num()
+
+/* Abstract class that generates a fractal by returning the appropriate color of a
+ * complex input
+*/
+class FractalGen {
+    public:
+
+        //FractalGen() {};
+        //~FractalGen() {};
+
+        virtual rgb_t color_complex_num(std::complex<double> num) = 0;
+};
+
+/* class that colors a complex number c according to the iterative rule z_n+1 = z_n + c
+ * where z_0 = 0
+ */
+class Mandelbrot : public FractalGen {
+
+    private:
+
+        rgb_t bckgrnd;
+        double r, g, b;
+
+    public:
+
+        // constructor
+        Mandelbrot(rgb_t bckgrnd, double r=1, double g=1, double b=1) {
+            this->bckgrnd = bckgrnd;
+            this->r = r;
+            this->g = g;
+            this->b = b;
+        }
+
+        rgb_t color_complex_num(std::complex<double> num){
+            std::complex<double> z(0, 0);                                       // start the 0-orbit
+            int cap = 1000;                                                     // set an iteration cap
+            int i;
+            for (i = 0; i < cap && abs(z) <= 2; i++){                           // iterate 0 on z_n+1 = z_n^2 + num
+                z = pow(z, 2) + num;
+            }
+            if (abs(z) <= 2) { return this->bckgrnd; }                          // if orbit has not diverged to infinity, return the background color
+            else {                                                              // otherwise, compute the scaled color
+                int r = (int)255*( 1/(double)i * this->r);
+                int g = (int)255*( 1/(double)i * this->g);
+                int b = (int)255*( 1/(double)i * this->b);
+                rgb_t color = make_colour(r, g, b);
+                return color;
+            }
+        }
+};
--- code/v1/main.cpp
+++ code/v1/main.cpp
@@ -2,29 +2,40 @@
 #include <sstream>
 
 #include <bitmap/bitmap_image.hpp>
-
 #include <cmath>
+#include <complex>
 
-#define WIDTH 640
-#define HEIGHT 480
-#define BCKGRND 66,252,190
-#define EPSILON 0.0001
+#include "fractal.h"
 
-void render(bitmap_image& image) {
-    // set bounding rectangle { l, r, b, t}
-    std::vector<float> bounds{ -5, 5, -5, 5 };
+// width and height define the resolution of the image
+#define WIDTH 1000
+#define HEIGHT 1000
+// left, right, top, and bottom define the window of the complex plane that we view
+#define LEFT -2
+#define RIGHT 2
+#define TOP 2
+#define BOTTOM -2
+// background color
+#define BCKGRND 0,0,0
+// fractal hue
+#define RED 0.1
+#define GREEN 0.3
+#define BLUE 1
+
+
+void render(bitmap_image& image, FractalGen* generator) {
+    double x_step = (double)(RIGHT - LEFT)/WIDTH;
+    double y_step = (double)(TOP - BOTTOM)/HEIGHT;
+    //std::cout << "x step: " << x_step << std::endl;
+    //std::cout << "y step: " << y_step << std::endl;
     // loop over all pixels
-    rgb_t background = make_colour(BCKGRND);
     for (unsigned int i = 0; i < WIDTH; i++) {
         for (unsigned int j = 0; j < HEIGHT; j++) {
+            std::complex<double> num(LEFT + i*x_step, BOTTOM + j*y_step);
             // compute convergence
-            int rate = 0;
+            rgb_t color = generator->color_complex_num(num);
             // set color
-            if (rate = 0) { image.set_pixel(i, j, background); }
-            else {
-                rgb_t color = make_colour(BCKGRND);
-                image.set_pixel(i, j, color);
-            }
+            image.set_pixel(i, j, color);
         }
     }
 }
@@ -33,9 +44,15 @@ int main(int argc, char** argv) {
 
     // create an image 640 pixels wide by 480 pixels tall
     bitmap_image image(WIDTH, HEIGHT);
+    rgb_t bckgrnd = make_colour(BCKGRND);
 
-    // render the world
-    render(image);
+    Mandelbrot mand(bckgrnd, RED, GREEN, BLUE);
+
+    FractalGen* fg = &mand;
+
+    // render the fractal
+    render(image, fg);
     image.save_image("../../../img/test.bmp");
     std::cout << "Success for test.bmp" << std::endl;
+    return 0;
 }
