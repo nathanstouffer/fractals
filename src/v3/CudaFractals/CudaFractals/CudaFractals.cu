8c4b2e209ca8afc5b7733c206a091381778d9dab
Nathan Stouffer <nathanstouffer1999@gmail.com>
2022-10-30 15:58:17 -0600

    add macro for detecting errors in a kernel function

--- src/v3/CudaFractals/CudaFractals/CudaFractals.cu
+++ src/v3/CudaFractals/CudaFractals/CudaFractals.cu
@@ -5,14 +5,24 @@
 #include <cuda_runtime_api.h>
 #include <cuda.h>
 
-// macro to catch cuda failures
-#define CUDA_ERROR(x) \
-{ \
+// macro to catch cuda api function failures
+#define DETECT_CUDA_ERROR(x) \
+{   /* scope so we can reuse this variable name */ \
     cudaError_t cudaErrorCode = ##x; \
     if (cudaErrorCode != cudaSuccess) \
     { \
         std::cout << "cuda function failed on line " << __LINE__ << " of " << __FILE__ << std::endl; \
         std::cout << "error: " << cudaGetErrorString(cudaErrorCode) << std::endl; \
+    } \
+}
+
+// macro to catch kernel function failures
+#define DETECT_KERNEL_ERROR(x, y) \
+{\
+    ##x,##y; \
+    if (cudaPeekAtLastError() != cudaSuccess) \
+    { \
+        std::cout << "kernel error: " << cudaGetErrorString(cudaGetLastError()); \
     } \
 }
 
@@ -34,32 +44,32 @@ int main()
     int* dSum;
 
     // allocate bytes on the gpu
-    CUDA_ERROR(cudaMalloc((void**)&dA, sizeof(int)));
-    CUDA_ERROR(cudaMalloc((void**)&dB, sizeof(int)));
-    CUDA_ERROR(cudaMalloc((void**)&dSum, sizeof(int)));
+    DETECT_CUDA_ERROR(cudaMalloc((void**)&dA, sizeof(int)));
+    DETECT_CUDA_ERROR(cudaMalloc((void**)&dB, sizeof(int)));
+    DETECT_CUDA_ERROR(cudaMalloc((void**)&dSum, sizeof(int)));
 
     // copy cpu bytes over to gpu
-    CUDA_ERROR(cudaMemcpy(dA, &hA, sizeof(int), cudaMemcpyHostToDevice));
-    CUDA_ERROR(cudaMemcpy(dB, &hB, sizeof(int), cudaMemcpyHostToDevice));
+    DETECT_CUDA_ERROR(cudaMemcpy(dA, &hA, sizeof(int), cudaMemcpyHostToDevice));
+    DETECT_CUDA_ERROR(cudaMemcpy(dB, &hB, sizeof(int), cudaMemcpyHostToDevice));
 
     // add the ints on the gpu
-    dAddInts<<<1, 1 >>>(dA, dB, dSum);
+    DETECT_KERNEL_ERROR(dAddInts<<<1, 1>>>(dA, dB, dSum));
 
     // copy the memory from the gpu to the cpu
-    CUDA_ERROR(cudaMemcpy(&hSum, dSum, sizeof(int), cudaMemcpyDeviceToHost));
+    DETECT_CUDA_ERROR(cudaMemcpy(&hSum, dSum, sizeof(int), cudaMemcpyDeviceToHost));
     
     // temp for debugging
     int tmp0;
     int tmp1;
-    CUDA_ERROR(cudaMemcpy(&tmp0, dA, sizeof(int), cudaMemcpyDeviceToHost));
-    CUDA_ERROR(cudaMemcpy(&tmp1, dB, sizeof(int), cudaMemcpyDeviceToHost));
+    DETECT_CUDA_ERROR(cudaMemcpy(&tmp0, dA, sizeof(int), cudaMemcpyDeviceToHost));
+    DETECT_CUDA_ERROR(cudaMemcpy(&tmp1, dB, sizeof(int), cudaMemcpyDeviceToHost));
     std::cout << tmp0 << std::endl;
     std::cout << tmp1 << std::endl;
 
     // free gpu memory
-    CUDA_ERROR(cudaFree(dA));
-    CUDA_ERROR(cudaFree(dB));
-    CUDA_ERROR(cudaFree(dSum));
+    DETECT_CUDA_ERROR(cudaFree(dA));
+    DETECT_CUDA_ERROR(cudaFree(dB));
+    DETECT_CUDA_ERROR(cudaFree(dSum));
 
     // print the output
     std::cout << "5 + 3 = " << hSum << std::endl;
