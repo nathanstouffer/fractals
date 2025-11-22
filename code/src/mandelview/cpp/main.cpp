72b75605cae2269c95094016d81e1ea92adeb5be
Nathan Stouffer <nathanstouffer1999@gmail.com>
2025-11-22 15:50:20 -0700

    set up CLI

--- code/src/mandelview/cpp/main.cpp
+++ code/src/mandelview/cpp/main.cpp
@@ -1,11 +1,22 @@
 #include "mandelview/renderer.hpp"
 
+#include <CLI/CLI.hpp>
+
 namespace mandelview
 {
 
     int main(int argc, char** argv)
     {
-        renderer r = renderer({ 255, 0, 0 });
+        CLI::App app{"mandelview is a tool the dynamically renders the mandelbrot set on the GPU.", "mandelview"};
+
+        std::array<uint8_t, 3> color;
+        app.add_option("-c,--color", color, "The color (0-255) assigned to diverging inputs. Format: R G B")
+            ->type_name("R G B")
+            ->capture_default_str();
+
+        CLI11_PARSE(app, argc, argv);
+
+        renderer r = renderer(color);
         int status = r.run();
         return status;
     }
